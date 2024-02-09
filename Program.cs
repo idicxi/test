c6756665345e99e03e63572f31a8509ac63f0117
4e6yPaLIIKa228 <alexander_pushkar@mail.ru>
2024-02-09 10:28:55 +0300

    первая фиксация

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число b: ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            
+            int b = Convert.ToInt32(Console.ReadLine());
+    
+            if (a > b) {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Ваше число:");
+                Console.ForegroundColor = ConsoleColor.Red;
+                
+                Console.WriteLine(a);
+            }
+            if (a < b)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Ваше число:");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(b);
+            }
+
+            Console.ReadKey();
+           
+        }
+    }
+}
